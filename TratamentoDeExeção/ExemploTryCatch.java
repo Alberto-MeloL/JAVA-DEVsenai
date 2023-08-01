7e76c54876ecb46962e5d0d3967a98c3c399da3a
alberto12345678900 <albertomeloteco@gmail.com>
2023-08-01 14:50:35 -0300

    01/08/2023

--- TratamentoDeExeção/ExemploTryCatch.java
+++ TratamentoDeExeção/ExemploTryCatch.java
@@ -2,17 +2,27 @@ package TratamentoDeExeção;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class ExemploTryCatch {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        try {
-            System.out.print("Digite um valor inteiro..:");
-            int numero1 = s.nextInt();
-            System.out.print("Digite um valor inteiro..:");
-            int numero2 = s.nextInt();
-            System.out.println(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
-        } catch (Exception e) {
-            System.out.println("ERRO - Valor digitado nao e um numero inteiro!");
+        boolean corrija = true;
+        int n1, n2;
+        String operacaoString[] = { "Soma", "Subtração", "Multiplicação", "Divisão", "Sair"};
+        while (corrija) {
+            try {
+                n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro:\n"));
+                n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número inteiro:\n"));
+                JOptionPane.showMessageDialog(null, "O resultado em soma é " + (n1 + n2));
+                JOptionPane.showMessageDialog(null, "O resultado em divisão é " + (n1 / n2));
+                JOptionPane.showMessageDialog(null, "O resultado em multiplicação é " + (n1 * n2));
+                JOptionPane.showMessageDialog(null, "O resultado em subtração é " + (n1 - n2));
+                corrija = false;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null,
+                        "ERRO - Valor digitado nao e um numero inteiro!\nDigite novamente.");
+            }
         }
     }
 }
