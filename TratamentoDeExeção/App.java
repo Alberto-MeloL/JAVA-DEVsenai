63b049e8f3d74d9f253893dc741ca48d6406e7c6
alberto12345678900 <albertomeloteco@gmail.com>
2023-08-02 13:50:00 -0300

    02/08/2023

---
+++ TratamentoDeExeção/App.java
@@ -0,0 +1,9 @@
+package TratamentoDeExeção;
+
+public class App {
+    public static void main(String[] args) {
+        ExemploTryCatch obj = new ExemploTryCatch();
+        obj.TratamentoDeExeção02();
+    }
+   
+}
--- TratamentoDeExeção/ExemploTryCatch.java
+++ TratamentoDeExeção/ExemploTryCatch.java
@@ -9,7 +9,7 @@ public class ExemploTryCatch {
         Scanner s = new Scanner(System.in);
         boolean corrija = true;
         int n1, n2;
-        String operacaoString[] = { "Soma", "Subtração", "Multiplicação", "Divisão", "Sair"};
+        String operacaoString[] = { "Soma", "Subtração", "Multiplicação", "Divisão", "Sair" };
         while (corrija) {
             try {
                 n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro:\n"));
@@ -22,6 +22,46 @@ public class ExemploTryCatch {
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null,
                         "ERRO - Valor digitado nao e um numero inteiro!\nDigite novamente.");
+            }
+        }
+    }
+
+    public void TratamentoDeExeção02() {
+
+        int n1, n2;
+        boolean corrija = true;
+        String operacoes[] = { "Soma", "Subtração", "Multiplicação", "Divisão", "Sair" };
+        while (corrija) {
+            try {
+                n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um nº Inteiro"));
+                n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro nº Inteiro"));
+                int i = JOptionPane.showOptionDialog(null, "Escolha a Operação Desejada", "Operação",
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.CANCEL_OPTION, null, operacoes, operacoes[0]);
+                if (i == 0) {// soma
+                    JOptionPane.showMessageDialog(null, "O resultado é \n"
+                            + (n1 + n2));
+                } else if (i == 1) {// sub
+                    JOptionPane.showMessageDialog(null, "O resultado é \n"
+                            + (n1 - n2));
+                } else if (i == 2) {// soma
+                    JOptionPane.showMessageDialog(null, "O resultado é \n"
+                            + (n1 * n2));
+                } else if (i == 3) {// divi
+                    JOptionPane.showMessageDialog(null, "O resultado é \n"
+                            + (n1 / n2));
+                } else {
+                    corrija = false;
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "ERRO - Valor "
+                        + "digitado não é um número inteiro!");
+            } catch (ArithmeticException e) {
+                JOptionPane.showMessageDialog(null, "ERRO - Aritmético "
+                        + "Não Dividirás por Zero!!!");
+            } catch (Exception e) {
+                // TODO: handle exception
+                JOptionPane.showMessageDialog(null, "ERRO - Valor");
             }
         }
     }
